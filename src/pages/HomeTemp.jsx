ef5bc5d72133d7241e39cec87373a17ed8d18655
mateusturola <turolamateus@gmail.com>
2022-01-13 17:34:52 -0300

    refactor(Ajusta Lint): :zap: Ajusta Lint

--- src/pages/HomeTemp.jsx
+++ src/pages/HomeTemp.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import ButtonConfig from '../components/ButtonConfig';
 import logo from '../trivia.png';
 
@@ -7,14 +8,19 @@ class HomeTemp extends Component {
     const { history } = this.props;
     return (
       <div>
-        {/* <header className="App-header">
+        <header className="App-header">
           <img src={ logo } className="App-logo" alt="logo" />
           <p>SUA VEZ</p>
-        </header> */}
+        </header>
         <ButtonConfig history={ history } />
       </div>
     );
   }
 }
 
+HomeTemp.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func.isRequired,
+  }).isRequired,
+};
 export default HomeTemp;
